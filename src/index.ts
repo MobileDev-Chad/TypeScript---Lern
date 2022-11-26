3a4dbd8821a730b9ca83d0ed91aa6bc8a0abd834
WebDev-Chad <chadwin.allotey@gmail.com>
2022-11-25 20:39:26 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
-let sales = 123_456_789;
-let course = "TypeScript";
-let is_published = true;
-let level;
+class Account {
+  readonly id: number;
+  owner: string;
+  balance: number;
+  nickname?: string;
+
+constructor(id: number, owner: string, balance: number) {
+  this.id=id;
+  this.owner=owner;
+  this.balance=balance;
+}
+
+deposit(amount: number):void {
+  if(amount <=0)
+  throw new Error('Invalid amount')
+  this.balance += amount;
+}
+}
+
+let account = new Account(1,'Chadwin', 0)
+account.deposit(100)
